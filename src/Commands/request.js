a1ebedd2ab90cba739c517ed1b7f1af2ec5a22d8
zTwuben <79409264+zTwuben@users.noreply.github.com>
2021-10-12 23:05:39 +0100

    Add files via upload

--- src/Commands/request.js
+++ src/Commands/request.js
@@ -2,6 +2,9 @@ const Command = require("../Structures/Command.js");
 
 const Discord = require("discord.js");
 
+const accept = '✅';
+const deny = '❌';
+
 module.exports = new Command({
     name: "request",
     description: "Makes a request",
@@ -26,48 +29,38 @@ module.exports = new Command({
         if(message.attachments.size > 0 ){
             msg.reply(message.attachments.first().url)
         }
-        message.delete();
-        msg.react('✅');
-        msg.react('❌');
+        await message.delete();
+        msg.react(accept);
+        msg.react(deny);
         console.log("Created request embed with reactions");
         
         const filter = (reaction, user) => {
-            return ["✅", "❌"].includes(reaction.emoji.name) &&
+            return [accept, deny].includes(reaction.emoji.name) &&
             message.guild.members.cache.get(user.id).roles.cache.find(r => r.id === "771439121368350760"); //Change role ID later on!
-            
         };
 
 
+        const collector = msg.createReactionCollector({ filter });
 
-        msg.awaitReactions({ filter, max: 1 })
-        .then(async collected =>  {
-            const reaction = collected.first()
-
-            if(reaction.emoji.name === '✅'){
+        collector.on('collect', async (reaction, user) =>{
+            
+            if (reaction.emoji.name === accept) {
                 const newmsg =  await logs.send({ embeds:  [embed] });
                 const member = reaction.users.cache.find((user) => !user.bot);
                 newmsg.reply({ content: `Request has been accepted by ${member}`}); 
 
-                newmsg.react('✅')
-                newmsg.awaitReactions({ filter, max: 1})
-                .then(async collected2 =>{
-                    const reaction2 = collected2.first()
+                newmsg.react(accept)
+                const collector2 = newmsg.createReactionCollector({ filter, max: 1 });
 
-                    if(reaction2.emoji.name === '✅'){
+                collector2.on('collect', async (reaction, user) =>{
+                    if(reaction.emoji.name === '✅'){
                         pickups.send({ content: `Request has been finished ${op}, pick it up here!`})
                     }
                 })
 
-
-            } else {
+            }else if(reaction.emoji.name === deny){
                 const Deniedmsg = msg.reply({ content: `Your request has been denied ${op}, have you followed the template?`});
-            };
-
-
-            
+            }
         })
     }
 })
-
-
-
