71f284f0d2888f9a93c7cc35a06c5186cc38b410
TwubenFR <djmixpr8@gmail.com>
2021-10-18 21:12:49 +0100

    TinyPresenceUpdate

--- src/Events/ready.js
+++ src/Events/ready.js
@@ -3,5 +3,8 @@ const Event = require("../Structures/Event.js");
 
 module.exports = new Event("ready", (client) => {
     console.log("Bot is online!")
-
+    client.user.setPresence({
+        activities: [{ name: 'Prefix is: -', type: 'COMPETING'}],
+        status: 'online',
+    })
 });
