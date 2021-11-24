0700bd62624a67c5c177a0e86b0ca24e59bdeb83
TwubenFR <djmixpr8@gmail.com>
2021-11-24 21:15:01 +0000

    Agree command push

---
+++ src/Commands/agree.js
@@ -0,0 +1,21 @@
+const Command = require("../Structures/Command.js");
+
+const Discord = require("discord.js");
+
+module.exports = new Command({
+    name: 'agree',
+    description: 'Gives verified role upon agreeing with rules!',
+    async run(message, args, client) {
+        //Example -agree
+
+        let verifiedRole = '892468150334029884'
+
+
+        //Errors
+        if (message.member.roles.cache.find(role => role.id === verifiedRole)) return message.channel.send('**You already have agreed with our rules**')
+
+        //Function
+
+        message.member.roles.add(verifiedRole)
+    }
+})
