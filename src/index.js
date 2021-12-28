4f8ce2bdf9381f20bc96d0547bda2dfb20aa0987
TwubenFR <djmixpr8@gmail.com>
2021-12-28 20:57:14 +0000

    Gitignore

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ console.clear();
 
 const Client = require("./Structures/Client.js");
 
-const config = require("./Data/config.json");
-
 const client = new Client();
 
 client.start(process.env.DJS_TOKEN);
