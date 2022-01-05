176850a934466df17fe129ef36e3d6bbbea9842a
TwubenFR <djmixpr8@gmail.com>
2022-01-05 21:59:17 +0000

    remove request v2

--- src/Commands/rr.js
+++ src/Commands/rr.js
@@ -12,10 +12,12 @@ module.exports = new Command({
 
         //Variables
         let messageArgs = args.splice(1).join(" ");
+        let roleID = '902656496867872799';
 
         
         //Errors
         if(!messageArgs) return message.channel.send('**Please specify a request ID**\nExample > *-rr __8782168798487951__*')
+        if (!message.member.roles.cache.find(role => role.id === roleID)) return message.channel.send('**You do not have the right permissions to remove requests!**')
        
         //Function
 
